9abaa94dd4dfa65406996accb0ebae4d98f8fbf6
sigenogoro <25719873+sigenogoro@users.noreply.github.com>
2024-12-10 10:04:39 +0900

    Farm Simulation プログラム作成

---
+++ src/main/java/org/learn/example/course/bmi/BMI.java
@@ -0,0 +1,25 @@
+package org.learn.example.course.bmi;
+
+// 人間や動物のBMI（Body Mass Index：肥満度を表す指数）を計算するクラス
+public class BMI{
+    private double heightM; // 身長
+    private double weightKg; // 体重
+
+    public BMI(double heightM, double weightKg){
+        this.heightM = heightM;
+        this.weightKg = weightKg;
+    }
+
+    public double getWeightKg(){
+        return this.weightKg;
+    }
+
+    public double getValue(){
+        return this.weightKg/(this.heightM*this.heightM);
+    }
+
+    public String toString(){
+        return this.heightM + " meters, " + this.weightKg + "kg, BMI:" + this.getValue();
+    }
+}
+
